ed7f8be28bcbafa8382078b9badfc336426bfbe9
see-quick <maros.orsak159@gmail.com>
2025-02-01 13:06:03 +0100

    Add simple add
    Signed-off-by: see-quick <maros.orsak159@gmail.com>

---
+++ assembly/SimpleAddition.s
@@ -0,0 +1,9 @@
+.text
+.global _main
+
+_main:
+	mov x0, #5
+	mov x1, #7
+	add x0, x0, x1
+	ret 
+
--- assembly/Start.s
+++ assembly/Start.s
@@ -2,8 +2,8 @@
 .align 2
 
 _main:
-	mov X0, #10 	// return 0
-	mov X16, #1	// terminate
+	mov x0, #10 	// return 0
+	mov x16, #1	// terminate
 
 	svc #0x80	// syscall
 
