25c9280bc90c25b8aebf75f05537c8065f4c76bc
see-quick <maros.orsak159@gmail.com>
2025-02-02 21:22:53 +0100

    a few things
    Signed-off-by: see-quick <maros.orsak159@gmail.com>

--- assembly/example_5.s
+++ assembly/example_5.s
@@ -3,16 +3,16 @@
 .data
 
 title:      .asciz  "GOSH I did it!!!"
-hwStr:      .asciz  "ManifiK! something string will be printed on stdout!!!!\n"
+hwStr:      .asciz  "ManifiK! something string will be printed on stdout!!!!"
 saveLR:     .dword  0       // Save LR here.
-            
+         
             .text
             
             .align  2       // Code must be 4-byte aligned
 getTitle:
             lea     x0, title
             ret
-            
+
 asmMain:
             lea     x0, saveLR
             str     lr, [x0]
